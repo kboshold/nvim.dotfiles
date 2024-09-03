d46fd5a5ab413da0701c9af048b2933123f49c38
Kevin Boshold <github@proxy.boshol.de>
2024-09-03 15:41:26 +0000

    Updated plugins

---
+++ lua/kboshold/config/plugins/ui/ccc.lua
@@ -0,0 +1,14 @@
+return {
+    "uga-rosa/ccc.nvim",
+    init = function() 
+        local ccc = require("ccc")
+        local mapping = ccc.mapping
+
+        ccc.setup({
+            highlighter = {
+                auto_enable = true,
+                lsp = true,
+            }
+        })
+    end
+}
