3099119589e46399343d8ee3fcb241caef7cd6ae
Kevin Boshold <github@proxy.boshol.de>
2024-09-16 19:42:41 +0200

    Fixed keybindings

--- lua/kboshold/config/plugins/ui/oil.lua
+++ lua/kboshold/config/plugins/ui/oil.lua
@@ -1,14 +1,29 @@
 return {
   'stevearc/oil.nvim',
-  opts = {
-    default_file_explorer = false,
-    delete_to_trash = true,
-    view_options = {
-      show_hidden = true
-    }
-  },
   -- Optional dependencies
   dependencies = {
     "nvim-tree/nvim-web-devicons"
-  }
+  },
+
+  config = function()
+    local oil = require('oil')
+
+    oil.setup({
+      default_file_explorer = false,
+      delete_to_trash = true,
+      view_options = {
+        show_hidden = true
+      },
+      float = {
+        padding = 6,
+        max_width = 100,
+        max_height = 120
+      }
+    });
+
+    -- keymap
+    vim.keymap.set('n', '<leader>od', function()
+      oil.open_float()
+    end, {})
+  end
 }
--- lua/kboshold/config/plugins/ui/telescope.lua
+++ lua/kboshold/config/plugins/ui/telescope.lua
@@ -6,14 +6,41 @@ return {
   },
 
   config = function()
-      require('telescope').setup({})
+    require('telescope').setup({})
 
-      -- TODO: Keymap
-      local builtin = require('telescope.builtin')
-      
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-      vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-  end
+    -- TODO: Keymap
+    local builtin = require('telescope.builtin')
+    local utils = require("telescope.utils")
+
+    -- find
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fd', function()
+      builtin.find_files({ 
+        cwd = utils.buffer_dir()
+      })
+    end, {})
+    vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fb', function() 
+      builtin.buffers({
+        sort_mru = true,
+        sort_lastused = true
+      })
+    end, {})
+    vim.keymap.set('n', '<leader>fr', function()
+      builtin.oldfiles({
+        cwd = vim.uv.cwd()
+      }) 
+    end, {})
+    vim.keymap.set('n', '<leader>fR', function()
+      builtin.oldfiles({
+        sort_mru = true,
+        sort_lastused = true
+      }) 
+    end, {})
+
+    -- search
+    vim.keymap.set('n', '<leader>s"', builtin.registers, {})
+
+  end,
 }
