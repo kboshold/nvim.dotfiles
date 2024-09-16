c9bfe59d4a42dc47063dfca231a7c26bce62b2d0
Kevin Boshold <github@proxy.boshol.de>
2024-09-16 21:46:03 +0200

    Updated oil

--- lua/kboshold/config/plugins/ui/oil.lua
+++ lua/kboshold/config/plugins/ui/oil.lua
@@ -15,15 +15,34 @@ return {
         show_hidden = true
       },
       float = {
-        padding = 6,
-        max_width = 100,
-        max_height = 120
-      }
+        padding = 2,
+        max_width = 140,
+        max_height = 50,
+        preview_split = "right",
+      },
     });
 
     -- keymap
+    local open_preview = false
     vim.keymap.set('n', '<leader>od', function()
-      oil.open_float()
+      open_preview = true
+      oil.toggle_float()
+
+      -- using OilEnter does not work quite well
+      vim.defer_fn(function()
+        oil.open_preview()
+      end, 50)
     end, {})
+
+    -- autocmd to have additional keymaps for preview
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "oil",
+      callback = function()
+        if vim.api.nvim_win_get_config(0).relative ~= "" then -- Check if the window is floating
+          vim.keymap.set('n', 'q', oil.toggle_float, { noremap = true, silent = true, buffer = true })
+          vim.keymap.set('n', '<ESC>', oil.toggle_float, { noremap = true, silent = true, buffer = true })
+        end
+      end,
+    })
   end
 }
