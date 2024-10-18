ea12c5d463c5e1f2a51f32ed4acf974eecff673e
Kevin Boshold <github@proxy.boshol.de>
2024-10-18 22:09:24 +0200

    Fixed trouble config

--- lua/kboshold/config/plugins/util/trouble.lua
+++ lua/kboshold/config/plugins/util/trouble.lua
@@ -1,21 +1,37 @@
 return {
 	"folke/trouble.nvim",
-	config = function()
-		require("trouble").setup({
-			icons = false,
-		})
-
-		vim.keymap.set("n", "<leader>tt", function()
-			require("trouble").toggle()
-		end)
-
-		vim.keymap.set("n", "[t", function()
-			require("trouble").next({skip_groups = true, jump = true});
-		end)
-
-		vim.keymap.set("n", "]t", function()
-			require("trouble").previous({skip_groups = true, jump = true});
-		end)
-
-	end
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>tt",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>tT",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>cs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>tL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>tQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+	}
 }
