0e9e3ef449a81144cb8b97516f3c639b0fbc393a
Kevin Boshold <github@proxy.boshol.de>
2025-04-13 21:51:17 +0200

    feat: add keymap for opening files at position
    Introduce a new keymap 'gf' to open files at the cursor
    position and navigate to specific lines or ranges.

--- lua/kboshold/config/keymaps.lua
+++ lua/kboshold/config/keymaps.lua
@@ -29,3 +29,43 @@ vim.keymap.set("n", "vih", function()
     vim.cmd("normal! `<v`>")
   end
 end, { desc = "View in Highlight Group" })
+
+vim.keymap.set("n", "gf", function()
+  local file = vim.fn.expand("<cfile>")
+  if file == "" or file == nil then
+    return
+  end
+
+  local line_content = vim.fn.getline(".")
+  local cursor_col = vim.fn.col(".")
+
+  local start_index = 1
+  local found_start = nil
+  local found_end = nil
+  while true do
+    found_start, found_end = string.find(line_content, file, start_index)
+    if not found_start then
+      break
+    end
+
+    if found_start <= cursor_col and found_end >= cursor_col then
+      break
+    end
+
+    start_index = found_end + 1
+  end
+
+  vim.cmd("e " .. file)
+
+  if found_end then
+    local text_after = string.sub(line_content, found_end + 1)
+    local line_start, _, line_end = string.match(text_after, "^:(%d+)(%-?(%d*))")
+    if line_start then
+      vim.cmd(line_start)
+    end
+
+    if line_end and line_end > 0 then
+      vim.cmd("normal! V" .. line_end .. "G")
+    end
+  end
+end, { desc = "Open file at position" })
