13f5ede7b177a071b8611c1dd28c8f577237df03
Kevin Boshold <github@proxy.boshol.de>
2025-06-24 22:36:50 +0200

    feat: configure formatters for JS/TS and Vue
    Set up explicit formatters for JavaScript, TypeScript, and Vue
    filetypes using eslint and eslint_d. Specify formatter commands,
    arguments, and working directory for improved formatting
    consistency.

--- lua/kboshold/plugins/editor/conform.lua
+++ lua/kboshold/plugins/editor/conform.lua
@@ -1,12 +1,27 @@
 return {
   "stevearc/conform.nvim",
   opts = {
-    format_on_save = function(bufnr)
-      local ignore_ft = { "javascript", "typescript", "javascriptreact", "typescriptreact", "vue" }
-      if vim.tbl_contains(ignore_ft, vim.bo[bufnr].filetype) then
-        return
-      end
-      return { timeout_ms = 1000, lsp_fallback = true }
-    end,
+    formatters_by_ft = {
+      vue = { "eslint_d", "eslint", lsp_format = "fallack" },
+      javascript = { "eslint_d", "eslint", lsp_format = "fallack" },
+      javascriptreact = { "eslint_d", "eslint", lsp_format = "fallack" },
+      typescript = { "eslint_d", "eslint", lsp_format = "fallack" },
+      typescriptreact = { "eslint_d", "eslint", lsp_format = "fallack" },
+    },
+    formatters = {
+      eslint = {
+        -- command = function()
+        --   local root = require("conform.util").root_file({ "package.json" })
+        --   if root then
+        --     return root .. "/node_modules/.bin/eslint"
+        --   end
+        --   return "eslint" -- Fallback to global eslint if not found
+        -- end,
+        command = "./node_modules/.bin/eslint",
+        args = { "--fix", "--stdin", "--stdin-filename", "$FILENAME" },
+        stdin = true,
+        cwd = require("conform.util").root_file({ "package.json" }),
+      },
+    },
   },
 }
