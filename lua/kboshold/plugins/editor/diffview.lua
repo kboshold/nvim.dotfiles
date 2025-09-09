c0a53d8448daf124439d77131663047c9a8fa715
Kevin Boshold <github@proxy.boshol.de>
2025-09-09 17:20:11 +0200

    feat: add Diffview keymaps for branch and staged diffs
    Introduce keybindings to quickly diff the current branch
    against origin/main and to view uncommitted changes using
    Diffview.

--- lua/kboshold/plugins/editor/diffview.lua
+++ lua/kboshold/plugins/editor/diffview.lua
@@ -2,4 +2,22 @@ return {
   "sindrets/diffview.nvim",
   cmd = { "DiffviewOpen", "DiffviewClose" },
   config = true,
+  keys = {
+    -- Diff current branch with origin/main
+    {
+      "<leader>gdm",
+      function()
+        require("diffview").open({ "origin/main" })
+      end,
+      desc = "Diff: current branch vs origin/main",
+    },
+    -- Diff uncommitted changes (working tree)
+    {
+      "<leader>gds",
+      function()
+        require("diffview").open({})
+      end,
+      desc = "Diff: uncommitted changes",
+    },
+  },
 }
